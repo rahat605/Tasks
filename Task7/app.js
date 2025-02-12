417f990790c9f479155beb20a83efc21891c5d8e
Rahat Ahmed <91186934+rahat605@users.noreply.github.com>
2025-02-12 14:54:14 +0600

    task7

---
+++ Task7/app.js
@@ -0,0 +1,23 @@
+// Calculate average Number
+let arr = [2,2,2,2,2];
+
+const arrayAverage = (arr) =>{
+    let total = 0;
+    for(let i=0; i<arr.length; i++){
+        total = total + arr[i];
+    }
+    return total/arr.length;
+}
+
+// Odd even finder
+const isEven = a => {
+    if(a%2==0){
+        console.log("The number",a,"is an even number");
+    }else{
+        console.log("The number",a,"is not an even number");
+    }
+}
+
+console.log(arrayAverage(arr));
+isEven(5);
+isEven(102);
