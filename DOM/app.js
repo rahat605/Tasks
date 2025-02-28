a4b79c40aa21abc3142294ba8aa465114df7e69c
rahat605 <mdrahatrion2@gmail.com>
2025-03-01 02:41:51 +0600

    dom

---
+++ DOM/app.js
@@ -0,0 +1,21 @@
+let para = document.createElement('p');
+para.innerText = "Hey I am Red!";
+document.querySelector('body').append(para);
+para.classList.add('red');
+
+let h3 = document.createElement('h3');
+h3.innerText = "I am a blue h3!";
+document.querySelector('body').append(h3);
+h3.classList.add('blue');
+
+let div = document.createElement('div');
+div.classList.add('border','background');
+document.querySelector('body').append(div);
+
+let h1 = document.createElement('h1');
+h1.innerText = "I'm in div";
+div.append(h1);
+
+let p2 = document.createElement('p');
+p2.innerText = "ME TOO!";
+div.append(p2);
